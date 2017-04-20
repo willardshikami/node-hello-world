88f08e745c9cb92b4f8a2c21bad240f81f7f14d8
willardshikami <willardshikami@gmail.com>
2017-04-20 11:29:19 +0300

    updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
 $(function(){
 
     var $items = $('#items');
-
+    
     $.ajax({
         type : 'GET',
         url : '/api/latest',
         success : function(items){
             $.each(items, function(i, items){
-                $items.append('<li>'+'Item: ' + items.title +'</li>')
+                $items.append('<li>'+'Name: ' + items.title +'</li>' + '<li>'+'Url: ' + items.url +'</li>' + '<br/>')
             });
         }
     });
