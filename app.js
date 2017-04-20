ce6c9e558686a4e8675ce86fe830ab2ac7398e35
willardshikami <willardshikami@gmail.com>
2017-04-20 09:11:17 +0300

    updated

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 var http = require('http');
-var path = require('path');
+app.use(express.static('public'));
+
 
 
 
 //environments
 app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded());
 
-app.use(bodyParser.json());
 
 //JSON api
 app.get('/api/latest', function(req, res){
     res.json([{
-        title: 'Backbone',
-        url : 'http://backbonejs.org/'
+        title: "Backbone",
+        url : "http://backbonejs.org/"
     },{
-        title : 'Angular',
-        url : 'https://angularjs.org/'
+        title : "Angular",
+        url : "https://angularjs.org/"
     },{
-        title : 'React',
-        url : 'https://facebook.github.io/react/'
+        title : "React",
+        url : "https://facebook.github.io/react/"
     }, {
-        title : 'Ember',
-        url : 'https://emberjs.com/'
+        title : "Ember",
+        url : "https://emberjs.com/"
     },{
-        title : 'Aurelia',
-        url : 'http://aurelia.io/'
+        title : "Aurelia",
+        url : "http://aurelia.io/"
     }
     
     ]);
 });
 
-app.get('/api/sample', function(req, res){
+app.get("/api/sample", function(req, res){
     res.json([{
-        title : 'Bootstrap',
-        description : 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        url : 'http://getbootstrap.com/'
+        title : "Bootstrap",
+        description : "Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.",
+        url : "http://getbootstrap.com/"
     }
 
     ]);
 });
+
 
 
 var server = http.createServer(app).listen(app.get('port'), function(){
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,14 @@
+$(function(){
 
-$(function (){
+    var $items = $('#items');
+
     $.ajax({
-
+        type : 'GET',
+        url : '/api/latest',
+        success : function(items){
+            $.each(items, function(i, items){
+                $items.append('<li>'+'Item: ' + item.title +'</li>')
+            });
+        }
     });
 });
