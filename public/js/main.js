046d5012148540a0c78455e4e59ede76dd200d9a
willardshikami <willardshikami@gmail.com>
2017-05-08 10:08:22 +0300

    updated the UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
 angular.module('resourceApp', [])
-.controller('ResourceAppController', function($scope, $http){
-    var resourceView = this;
-    resourceView.loadResources = function(){
-        $http({
-            url: 'api/resources',
-            method: 'GET'
-        }).then(function(resources){
-            $scope.resources = resources.data;
-        });
-    };
-});
+    .controller('ResourceAppController', function ($scope, $http) {
+        var resourceView = this;
+        resourceView.loadResources = function () {
+            $http({
+                url: 'api/resources',
+                method: 'GET'
+            }).then(function (resources) {
+                $scope.resources = resources.data;
+            });
+        };
+    });
--- server.js
+++ server.js
@@ -11,39 +11,33 @@ app.set('port', process.env.PORT || 3000);
 
 
 //JSON api
-app.get('/api/resources', function(req, res){
+app.get('/api/resources', function (req, res) {
     res.json([{
-        title: "Backbone",
-        url : "http://backbonejs.org/"
-    },{
-        title : "Angular",
-        url : "https://angularjs.org/"
-    },{
-        title : "React",
-        url : "https://facebook.github.io/react/"
-    }, {
-        title : "Ember",
-        url : "https://emberjs.com/"
-    },{
-        title : "Aurelia",
-        url : "http://aurelia.io/"
-    }
-    
-    ]);
-});
-
-app.get("/api/sample", function(req, res){
-    res.json([{
-        title : "Bootstrap",
-        description : "Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.",
-        url : "http://getbootstrap.com/"
-    }
+            title: "Backbone",
+            url: "http://backbonejs.org/"
+        }, {
+            title: "Angular",
+            url: "https://angularjs.org/"
+        }, {
+            title: "React",
+            url: "https://facebook.github.io/react/"
+        }, {
+            title: "Ember",
+            url: "https://emberjs.com/"
+        }, {
+            title: "Aurelia",
+            url: "http://aurelia.io/"
+        }, {
+            title: "Bootstrap",
+            description: "Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.",
+            url: "http://getbootstrap.com/"
+        }
 
     ]);
 });
 
 
 
-var server = http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' +app.get('port'));
+var server = http.createServer(app).listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
 });
