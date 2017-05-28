d37f4da2612465675caaa201c04e59f2962d2212
willardshikami <willardshikami@gmail.com>
2017-05-28 17:01:29 +0300

    line-clap on long paragraphs

--- server.js
+++ server.js
@@ -16,35 +16,34 @@ mongoose.connect('');
 var db = mongoose.connection;
 
 //checking for connection to the DB
-db.on('error',console.error.bind(console, 'connection error:'));
-db.once('open', function(){
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
     console.log('We are connected');
 });
 
 //Defining the Schema
 var resourceSchema = mongoose.Schema({
-    title :{
-        type : String,
-        required : true
+    title: {
+        type: String,
+        required: true
     },
-    description :{
-        type : String,
-        required : true
+    description: {
+        type: String,
+        required: true
     },
-    url:{
-        type : String,
+    url: {
+        type: String,
         required: true
     },
 })
 
 var resource = mongoose.model('resource', resourceSchema);
 
-app.get('/api/resources', function (req, res) {    
-    resource.find().exec(function (err, resources)
-     {
-         res.json(resources);
-        });
+app.get('/api/resources', function (req, res) {
+    resource.find().exec(function (err, resources) {
+        res.json(resources);
     });
+});
 
 
 var server = http.createServer(app).listen(app.get('port'), function () {
